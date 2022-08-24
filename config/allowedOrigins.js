79613a5b33e8b263ddbb9c09f25d1e00f5faac0a
michaelOch <ochuenwikechukwudi@hotmail.com>
2022-08-24 22:18:56 +0200

    chore: add configurations and setup database

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.abclogistics.com',
+    'https://abclogistics.com'
+]
+
+module.exports = allowedOrigins;
---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    roles: [{
+        type: String,
+        default: "user"
+    }],
+    tel: {
+        type: String
+    },
+    address: {
+        type: String
+    },
+    active: {
+        type: Boolean,
+        default: true
+    },
+});
+
+module.exports = mongoose.model('User', userSchema);
